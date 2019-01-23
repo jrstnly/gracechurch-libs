6b544e685b28b6cdc4d248588ec079d0bd51af1d
jrstnly <jr@jrstn.ly>
2019-01-22 21:40:58 -0600

    Changed mysql to root class

--- src/DatabaseConnect.php
+++ src/DatabaseConnect.php
@@ -16,7 +16,7 @@ class DatabaseConnect {
     function connect($db_name) {
 
         // Connecting to mysql database
-        $this->conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, $db_name);
+        $this->conn = new \mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, $db_name);
 
         // Check for database connection error
         if (mysqli_connect_errno()) {
